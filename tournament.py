ed7a50b10e8ce7fd82753f82a4f45551526c42e7
Ifeoluwapo Kazeem Oluwande <ioluwande@gmail.com>
2021-12-05 08:21:23 +0100

    DictReader already read as dict, teams.append(row)

--- tournament.py
+++ tournament.py
@@ -19,7 +19,8 @@ def main():
     with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
         for row in reader:
-            teams.append({"team":row["team"], "rating": int(row["rating"])})
+            row["rating"] = int(row["rating"])
+            teams.append(row)
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
